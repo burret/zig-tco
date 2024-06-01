d57882ae6cb7f3b6d7d3c7acab34538c800c8415
burret <32988772+burret@users.noreply.github.com>
2024-06-01 16:53:20 -0700

    fibonacci: switch from stdout to stderr

--- fibonacci.zig
+++ fibonacci.zig
@@ -6,8 +6,9 @@ const std = @import("std");
 // aarch64-macos.14.5...14.5-none
 // $ zig build-exe fibonacci.zig
 // $ ./fibonacci 185
-// 205697230343233228174223751303346572685
-// $
+// info: 205697230343233228174223751303346572685
+// $ ./fibonacci 186
+// thread 5900199 panic: integer overflow
 
 fn tail_call_fib(i: u128, c: u128, n: u128) u128 {
     return if (i == 0) c else @call(.always_tail, tail_call_fib, .{i - 1, n, c + n});
@@ -18,10 +19,9 @@ pub fn fib(i: u128) u128 {
 }
 
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
     const args = try std.process.argsAlloc(allocator);
     const index: u128 = try std.fmt.parseInt(u128, args[1], 10);
-    try stdout.print("{d}\n", .{fib(index)});
+    std.log.info("{d}", .{fib(index)});
 }
